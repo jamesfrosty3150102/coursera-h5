0eb2f6682e7c17e9ac50d9449b656f9b5087ebce
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2018-02-14 11:17:22 +0800

    correction
    correction

--- js/script.js
+++ js/script.js
@@ -22,12 +22,3 @@ document.addEventListener("DOMContentLoaded",
   }
 );
 
-// for textbox
-function prettyPrint() {
-    var ugly = document.getElementById('myTextArea').value;
-    var obj = JSON.parse(ugly);
-    var pretty = JSON.stringify(obj, undefined, 4);
-    document.getElementById('myTextArea').value = pretty;
-}
-
-
