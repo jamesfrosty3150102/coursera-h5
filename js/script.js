43caf403684326a5cc51b35963b96c5ba91fe460
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2018-02-14 11:08:28 +0800

    textbox testing
    textbox testing

--- js/script.js
+++ js/script.js
@@ -22,7 +22,12 @@ document.addEventListener("DOMContentLoaded",
   }
 );
 
-
-
+// for textbox
+function prettyPrint() {
+    var ugly = document.getElementById('myTextArea').value;
+    var obj = JSON.parse(ugly);
+    var pretty = JSON.stringify(obj, undefined, 4);
+    document.getElementById('myTextArea').value = pretty;
+}
 
 
