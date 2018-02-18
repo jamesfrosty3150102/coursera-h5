168952774949a2295ff9faa2d8d50f6f16cd12c0
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2018-02-18 15:35:10 +0800

    testing
    testing

--- js/script.js
+++ js/script.js
@@ -1,31 +1,10 @@
+<div id="textDiv">  
+    <div id="nested"></div>  
+</div>  
+<script type="text/javascript">  
+    var div = document.getElementById("textDiv");  
+    var nestedDiv = document.getElementById("nested");  
+    nestedDiv.textContent = "nested";  
 
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-    
-    // Unobtrusive event binding
-    document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/name.json", 
-            function (res) {
-              var message = 
-                res.firstName + " " + res.lastName
-              message += res.numberOfDisplays + 1;
-
-              document.querySelector("#content")
-                .innerHTML = "<h2>" + message + "</h2>";
-            });
-      });
-  }
-);
-
-// taking value test
-  <script type="text/javascript">
-   var txt;
-   txt = document.getElementById('txta').value; //获取textarea的值
-  document.write (txt);
-   document.getElementById('txta').value = "txt2";  //设置textarea的值
-</script>
+    var text = "[" + div.textContent + "]";  
+</script>  
