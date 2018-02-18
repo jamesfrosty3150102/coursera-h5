9482889f1ed6a2ffa932746dae870de2bf1bc9d4
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2018-02-19 02:44:27 +0800

    clear
    clear

--- js/script.js
+++ js/script.js
@@ -12,16 +12,7 @@ document.addEventListener("DOMContentLoaded",
             function (res) {
               var message = 
                 res.firstName + " " + res.lastName
-              if (res.likesChineseFood) {
-                message += " likes Chinese food";
-              }
-              else {
-                message += " doesn't like Chinese food";
-              }
-              message += " and uses ";
-              message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
-
+              
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
             });
