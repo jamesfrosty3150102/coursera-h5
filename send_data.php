d067a68fcc5c54e83cdb1cb8ea9bd44a8789fcbf
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2018-02-21 00:14:19 +0800

    php testing
    php testing

--- send_data.php
+++ send_data.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://jamesfrosty3150102.github.io/coursera-h5/index.html"></script>
-  <script language="javascript" type="text/javascript">
-                var scrt_var = 25; 
-                openPage = function() {
-                location.href = "http://api.thingspeak.com/update?key=D6TS14SPWILON2WU&field1="+scrt_var;}
-  </script> 
-<head>
-</head>
-  <body>
-       <form action="https://jamesfrosty3150102.github.io/coursera-h5/index.html" method="get">
-       your txtfirstname:  $_GET[txtfirstname]; <br />
-       your lblfirstname:  $_GET[lblfirstname]; <br />
-    </form>
-       <?php echo ("第二種方法！"); ?>
-       sending iot <a href ="javascript:openPage()">link </a>
-    
-
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo "<p>Hello World</p>"; ?>
  </body>
 </html>
-
